e311bbccc8eb9b776ca1bd71e84bcfa410a7b3c6
AILabDsUnipi <gp.papadopoulos.george@gmail.com>
2023-01-30 13:56:31 +0100

    Automate installation of requirements

---
+++ install_requirements.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+conda install -y pandas=1.1.5
+conda install -y numba=0.50
+conda install -y pyproj=2.6.1.post1
+conda install -y cython=0.29.24
+yes | pip install geo-py==0.4
+conda install -y shapely=1.7.1
+conda install -y matplotlib=3.1.1
+conda install -y scipy=1.5
+conda install -y pymongo=3.12.0
+conda install -y tensorflow-gpu=1.14
+yes | pip install Keras==2.1.2
+conda install -y h5py=2.10.0
+conda install -y numpy==1.19.2
+conda install -y psutil==5.8.0
